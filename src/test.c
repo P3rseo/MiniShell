a56174c392028f84343333e1f822040690bc196a
mortiz <ortizlopomario.6@gmail.com>
2025-12-03 19:30:22 +0100

    PIPESç

--- src/test.c
+++ src/test.c
@@ -2,47 +2,97 @@
 #include <unistd.h>
 #include "parser.h"
 #include <sys/wait.h>
+#include <fcntl.h>
 
-int main(void) {
+int main(void)
+{
 	char buf[1024];
-	tline * line;
-	int i,j;
+	tline *line;
 
 	printf("msh>");
-	while (fgets(buf, 1024, stdin)) {
+	while (fgets(buf, 1024, stdin))
+	{
 
 		line = tokenize(buf);
-		if (line==NULL) {
+		if (line == NULL)
+		{
 			continue;
 		}
-		if (line->redirect_input != NULL) {
-			printf("redirección de entrada: %s\n", line->redirect_input);
-			freopen(line->redirect_input, "r", stdin);
-		}
-		if (line->redirect_output != NULL) {
-			printf("redirección de salida: %s\n", line->redirect_output);
-		}
-		if (line->redirect_error != NULL) {
-			printf("redirección de error: %s\n", line->redirect_error);
-		}
-		if (line->background) {
+
+		if (line->background)
+		{
 			printf("comando a ejecutarse en background\n");
-		} 
-		for (i=0; i<line->ncommands; i++) {
-			printf("orden %d (%s):\n", i, line->commands[i].filename);
-			for (j=0; j<line->commands[i].argc; j++) {
-				printf("  argumento %d: %s\n", j, line->commands[i].argv[j]);
-			}
-			int pid = fork();
-				if(pid<0){
-					fprintf(stderr, "Error en en el fork");
-				}else if(pid==0){
-					execvp(line->commands[i].filename, line->commands[i].argv);
-				}else{
-					wait(NULL);
-				}
 		}
-		printf("msh>");	
+
+
+
+		//ARRAY DE PIDS
+		pid_t pid[line->ncommands];
+
+
+
+		// CREAR LOS PIPES
+		if(line->ncommands>1){
+			int array_pipes[line->ncommands - 1][2];
+
+			// INICIALIZAR LOS PIPES
+			for (int i = 0; i < line->ncommands - 1; i++){
+				pipe(array_pipes[i]);
+			}
+		}
+
+
+
+
+		//CREAR LOS PROCESOS HIJOS
+		for (int i = 0; i < line->ncommands; i++)
+		{
+			pid[i] = fork();
+
+			if (pid[i] < 0)
+			{
+				fprintf(stderr, "Error en en el fork");
+			}
+			else if (pid[i] == 0)
+			{
+				if(i==0){
+					if (line->redirect_input != NULL) // STDIN
+					{
+						int fdin = open(line->redirect_input, O_RDONLY);
+						dup2(fdin, STDIN_FILENO);
+					}
+					if(line->ncommands>1){
+						close(array_pipes[i][0]); //CERRAR EL PRIMER PIPE PARA LECTURA
+						dup2(array_pipes[i][1], STDIN_FILENO);	//REDIRIGIR LA SALIDA ESTANDAR A LA POSICION DE ESCRITURA DEL PRIMER PIPE
+					}
+				}else if(i==line->ncommands-1){
+					if (line->redirect_output != NULL) // STDOUT
+					{
+						int fdout = open(line->redirect_output, O_WRONLY);
+						dup2(fdout, STDOUT_FILENO);
+					}
+					if (line->redirect_error != NULL) // STDERR
+					{
+						int fderr = open(line->redirect_error, O_WRONLY);
+						dup2(fderr, STDERR_FILENO);
+					}
+
+					close(array_pipes[i][1]);	//CERRAR EL ULTIMO PIPE PARA ESCRITURA
+				}else{
+
+				}
+
+
+				execvp(line->commands[i].filename, line->commands[i].argv);
+				printf("SIGGO VIVO");
+			}
+		}
+		for (int i = 0; i < line->ncommands; i++)
+		{
+			wait(NULL);
+		}
+		dup2(STDIN_FILENO, STDERR_FILENO);
+		printf("msh>");
 	}
 	return 0;
 }
