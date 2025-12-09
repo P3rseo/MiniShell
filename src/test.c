9b319d0cb0defb96aa4ea3c55528eb9caf770941
P3rseo <samulopezc11@gmail.com>
2025-12-09 17:45:04 +0100

    pipes funcionando prueba

--- src/test.c
+++ src/test.c
@@ -3,6 +3,7 @@
 #include "parser.h"
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -24,80 +25,105 @@ int main(void)
 			printf("comando a ejecutarse en background\n");
 		}
 
+		int ncmds = line->ncommands;
+		pid_t pid[ncmds];
 
-
-		//ARRAY DE PIDS
-		pid_t pid[line->ncommands];
-
-
-		
-		// CREAR LOS PIPES
-		int array_pipes[line->ncommands - 1][2];
-		if(line->ncommands>1){
-			
-
-			// INICIALIZAR LOS PIPES
-			for (int i = 0; i < line->ncommands - 1; i++){
-				pipe(array_pipes[i]);
+		int npipes = ncmds - 1;
+		int pipes[npipes][2];
+		for (int i = 0; i < npipes; i++)
+		{
+			if (pipe(pipes[i]) < 0)
+			{
+				perror("pipe");
+				return EXIT_FAILURE;
 			}
 		}
 
-
-
-
-		//CREAR LOS PROCESOS HIJOS
-		for (int i = 0; i < line->ncommands; i++)
+		for (int i = 0; i < ncmds; i++)
 		{
 			pid[i] = fork();
-
 			if (pid[i] < 0)
 			{
-				fprintf(stderr, "Error en en el fork");
+				perror("fork");
+				return EXIT_FAILURE;
 			}
 			else if (pid[i] == 0)
 			{
-				if(i==0){
-					if (line->redirect_input != NULL) // STDIN
+				if (i == 0 && line->redirect_input != NULL)
+				{
+					int fdin = open(line->redirect_input, O_RDONLY);
+					if (fdin < 0 || dup2(fdin, STDIN_FILENO) < 0)
 					{
-						int fdin = open(line->redirect_input, O_RDONLY);
-						dup2(fdin, STDIN_FILENO);
+						perror("redirect input");
+						_exit(EXIT_FAILURE);
 					}
-					if(line->ncommands>1){
-						close(array_pipes[i][0]); //CERRAR EL PRIMER PIPE PARA LECTURA
-						dup2(array_pipes[i][1], STDOUT_FILENO);	//REDIRIGIR LA SALIDA ESTANDAR A LA POSICION DE ESCRITURA DEL PRIMER PIPE
-					}
-				}else if(i==line->ncommands-1){
-					if (line->redirect_output != NULL) // STDOUT
-					{
-						int fdout = open(line->redirect_output, O_WRONLY);
-						dup2(fdout, STDOUT_FILENO);
-					}
-					if (line->redirect_error != NULL) // STDERR
-					{
-						int fderr = open(line->redirect_error, O_WRONLY);
-						dup2(fderr, STDERR_FILENO);
-					}
-
-					close(array_pipes[i-1][1]);	//CERRAR EL ULTIMO PIPE PARA ESCRITURA
-					dup2(array_pipes[i-1][0], STDIN_FILENO);
-
-				}else{
-					close(array_pipes[i-1][1]);
-					close(array_pipes[i][0]);
-					dup2(array_pipes[i-1][0], STDIN_FILENO);
-					dup2(array_pipes[i][1], STDOUT_FILENO);
+					close(fdin);
 				}
 
+				if (i == ncmds - 1)
+				{
+					if (line->redirect_output != NULL)
+					{
+						int fdout = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+						if (fdout < 0 || dup2(fdout, STDOUT_FILENO) < 0)
+						{
+							perror("redirect output");
+							_exit(EXIT_FAILURE);
+						}
+						close(fdout);
+					}
+					if (line->redirect_error != NULL)
+					{
+						int fderr = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+						if (fderr < 0 || dup2(fderr, STDERR_FILENO) < 0)
+						{
+							perror("redirect error");
+							_exit(EXIT_FAILURE);
+						}
+						close(fderr);
+					}
+				}
+
+				if (i > 0)
+				{
+					if (dup2(pipes[i - 1][0], STDIN_FILENO) < 0)
+					{
+						perror("dup2 stdin");
+						_exit(EXIT_FAILURE);
+					}
+				}
+				if (i < ncmds - 1)
+				{
+					if (dup2(pipes[i][1], STDOUT_FILENO) < 0)
+					{
+						perror("dup2 stdout");
+						_exit(EXIT_FAILURE);
+					}
+				}
+
+				for (int j = 0; j < npipes; j++)
+				{
+					close(pipes[j][0]);
+					close(pipes[j][1]);
+				}
 
 				execvp(line->commands[i].filename, line->commands[i].argv);
-				printf("SIGGO VIVO");
+				perror("execvp");
+				_exit(EXIT_FAILURE);
 			}
 		}
-		for (int i = 0; i < line->ncommands; i++)
+
+		for (int i = 0; i < npipes; i++)
 		{
-			wait(NULL);
+			close(pipes[i][0]);
+			close(pipes[i][1]);
 		}
-		dup2(STDIN_FILENO, STDERR_FILENO);
+
+		for (int i = 0; i < ncmds; i++)
+		{
+			waitpid(pid[i], NULL, 0);
+		}
+
 		printf("msh>");
 	}
 	return 0;
